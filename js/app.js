bd977edb06aab094111f43fa8ba29c188a1adfa3
LCC CORP <lcccorpmx@gmail.com>
2022-09-27 13:03:51 -0500

    first commit

---
+++ js/app.js
@@ -0,0 +1,25 @@
+const seccionesPagina = new fullpage('#fullpage', {
+    autoScrolling: true, // Se activa el scroll.
+    fitToSection: false, // Acomoda el scroll automaticamente para que la seccion se muestre en pantalla.
+    fitToSectionDelay: 300, // Delay antes de acomodar la seccion automaticamente.
+    easing: 'easeInOutCubic', // Funcion de tiempo de la animacion.
+    scrollingSpeed: 700, // Velocidad del scroll. Valores: en milisegundos.
+    css3: true, // Si usara CSS3 o javascript.
+    easingcss3: 'ease-out', // Curva de velocidad del efecto.
+    loopBottom: false, // Regresa a la primera seccion siempre y cuando se ya haya llegado a la ultima sección y el ususario siga scrolleando.
+
+
+
+
+    navigation: true, // Muesta la barra de navegación.
+    menu: '#menu', // Menu de navegación.
+    anchors: ['inicio', 'promos','videos', 'infografias','crew','contacto'], // Anclas, las usamos para identificar cada seccion y poder acceder a ellas con el menu.
+    navigationTooltips: ['Inicio', 'Promos', 'videos', 'infografias','crew','Contacto'], // Tooltips que mostrara por cada boton.
+    showActiveTooltip: false, // Mostrar tooltip activa.
+
+
+    sectionsColor : ['#000', '#000', '#000', '#000', '#000' ,'#000'], // Color de fondo de cada seccion.
+    verticalCentered: true, // Si alineara de forma vertical los contenidos de cada seccion.
+
+
+});
---
+++ js/opcionesSinComentarios.js
@@ -0,0 +1,49 @@
+var seccionesPagina = new fullpage('#fullpage', {
+	autoScrolling: true,
+	fitToSection: false,
+	fitToSectionDelay: 300,
+	easing: 'easeInOutCubic',
+	scrollingSpeed: 700,
+	css3: true,
+	easingcss3: 'ease-out',
+	loopBottom: false,
+	navigation: true,
+	menu: '#menu',
+	anchors: ['inicio','promos' ,'videos','infografias','crew','contacto'],
+	navigationTooltips: ['Inicio','promos','videos','infografias','crew','Contacto'],
+	showActiveTooltip: false,
+	sectionsColor : ['#000', '#c2c2c2', '#c2c2c2', '#000', '#000'],
+	verticalCentered: true,
+	controlArrows: true,
+	slidesNavigation: false,
+	afterLoad: function(origin, destination){
+		if(destination.anchor == 'contacto'){
+			document.querySelector('.footer').querySelector('h2').style.opacity = 1;
+		}
+	}
+});
+
+
+
+<section class="section videos">
+<article class="slide">
+	<div class="infoProducto video">
+		<video src="/2.mp4" loop autoplay controls muted></video>
+	</div>
+</article>
+<article class="slide">
+	<div class="infoProducto video">
+		<video src="/3.mp4" loop autoplay controls muted></video>
+	</div>
+</article>
+<article class="slide">
+	<div class="infoProducto video">
+		<video src="/4.mp4" loop autoplay controls muted></video>
+	</div>
+</article>
+<article class="slide">
+   <div class="infoProducto video">
+	   <video src="/1.mp4" loop autoplay controls muted></video>
+   </div>
+</article>
+</section>
