57533dc750e6f4fac6b99f89239d2dad3433fe05
LCC CORP <lcccorpmx@gmail.com>
2022-11-16 14:34:33 -0600

    add new changes

--- js/app.js
+++ js/app.js
@@ -13,12 +13,12 @@ const seccionesPagina = new fullpage('#fullpage', {
 
     navigation: true, // Muesta la barra de navegación.
     menu: '#menu', // Menu de navegación.
-    anchors: ['inicio', 'promos','crew','contacto'], // Anclas, las usamos para identificar cada seccion y poder acceder a ellas con el menu.
-    navigationTooltips: ['Inicio', 'Promos','crew','Contacto'], // Tooltips que mostrara por cada boton.
-    showActiveTooltip: false, // Mostrar tooltip activa.
+    anchors: ['inicio','promos','crew','playlist','contacto'], // Anclas, las usamos para identificar cada seccion y poder acceder a ellas con el menu.
+    navigationTooltips: ['Inicio','Promos','crew','playlist','Contacto'], // Tooltips que mostrara por cada boton.
+    showActiveTooltip: true, // Mostrar tooltip activa.
 
 
-    sectionsColor : ['#000', '#000', '#000', '#000', '#000' ,'#000'], // Color de fondo de cada seccion.
+    sectionsColor : ['#000', '#000', '#000', '#000', '#000'], // Color de fondo de cada seccion.
     verticalCentered: true, // Si alineara de forma vertical los contenidos de cada seccion.
 
 
